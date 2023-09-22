fbda45b70364aaf86531d3f4eaec6c76f0acc91e
Maximilian Schumacher <schummax@icloud.com>
2023-09-22 14:35:32 +0200

    added avererage length calc

--- Python/scripts/lenght_dist.py
+++ Python/scripts/lenght_dist.py
@@ -26,7 +26,7 @@ dnamollen['total'] = dnamollen.groupby('sample')['count'].transform('sum')
 dnamollen['frac'] = (dnamollen['count'] / dnamollen['total']) * 100
 
 # Specify the order of samples for the legend
-sample_order = ["0_NA_B", "1_11_P", "1_16_K", "2_12_L", "2_16_N", "2_19_R", "3_06_Q", "3_16_M", "3_19_O"]
+sample_order = ['0_NA_B', '1_11_P', '1_16_K', '2_12_L', '2_16_N', '2_19_R', '3_06_Q', '3_16_M', '3_19_O']
 
 sns.set(font_scale=1.1)
 
@@ -49,3 +49,23 @@ for spine in ax.spines.values():
 
 # Ensure the background is white when saving
 plt.savefig('length_dist.jpg', dpi=600)  # facecolor='white'
+
+dnamollen_mean = {}
+sample_count = {}
+sample_length = {}
+# iterate over the rows of the DataFrame and multiply the count by the length
+for sample in sample_order:
+    print(sample)
+    dnamollen_mean[sample] = 0
+    sample_count[sample] = 0
+    sample_length[sample] = 0
+    for index, row in dnamollen.iterrows():
+        if row['sample'] == sample and row['length'] != 512: # exclude the 512 bp molecules as they are not real (possible artefact)
+            sample_count[sample] += row['count']
+            sample_length[sample] += row['count'] * row['length']
+        else:
+            continue
+    #calculate the mean
+    dnamollen_mean[sample] = sample_length[sample] / sample_count[sample]
+
+    print(dnamollen_mean[sample])
